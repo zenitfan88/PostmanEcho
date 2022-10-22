0335e24a2dbcd916e4517022ec392e6b1a4e5763
zenitfan88 <nikanenkov@rambler.ru>
2022-10-22 21:02:32 +0400

    init commit

---
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -0,0 +1,30 @@
+package ru.netology.test;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+
+    @Test
+    void schouldPostmanEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("headers.content-type", equalTo("text/plain; charset=UTF-8"))
+                .body ("headers.content-length",equalTo("9"))
+                .body("data", equalTo("some data"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11.0.14.1)"))
+                .body(matchesJsonSchemaInClasspath("postman.schema.json"))
+        ;
+    }
+}
